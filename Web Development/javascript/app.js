ccb4baabed609f0df3c8d1af56b3f1e94b690d85
animeshjain9 <jainani@ccs.neu.edu>
2015-02-22 10:43:59 -0500

    Advanced Angular

---
+++ Web Development/javascript/app.js
@@ -0,0 +1,35 @@
+﻿//Define an angular module for our app
+var sampleApp = angular.module('sampleApp', []);
+
+//Define Routing for app
+//Uri /AddNewOrder -> template AddOrder.html and Controller AddOrderController
+//Uri /ShowOrders -> template ShowOrders.html and Controller AddOrderController
+sampleApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/AddNewOrder', {
+            templateUrl: '/story/add_order.html',
+            controller: 'AddOrderController'
+        }).
+        when('/ShowOrders', {
+            templateUrl: '/story/show_orders.html',
+            controller: 'ShowOrdersController'
+        }).
+        otherwise({
+            redirectTo: '/AddNewOrder'
+        });
+  }]);
+
+
+sampleApp.controller('AddOrderController', function ($scope) {
+
+    $scope.message = 'This is the page where user can order things and add more things in the inventory';
+
+});
+
+
+sampleApp.controller('ShowOrdersController', function ($scope) {
+
+    $scope.message = 'This is the page where user can view the things which has been ordered already';
+
+});
---
+++ Web Development/javascript/app1.js
@@ -0,0 +1,18 @@
+﻿var sampleApp = angular.module('sampleApp_route', []);
+
+sampleApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/ShowOrder/:orderId', {
+            templateUrl: '/story/show_order1.html',
+            controller: 'ShowOrderController'
+        });
+  }]);
+
+
+sampleApp.controller('ShowOrderController', function ($scope, $routeParams) {
+
+
+    $scope.order_id = $routeParams.orderId;
+
+});
